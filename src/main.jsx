8dcc956454c7f2ecef34e4c14e38ddafa502e3f3
Tiago-S-Oliveira <tiago.nv.97@gmail.com>
2023-02-06 21:09:56 -0300

    aula-06-02-23

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+
+// import './index.css'  pra não dar conflito com o bootstrap
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement:<h1>erro 404 (Página não existe)</h1>,
+    children:[
+      {
+        path: "/",
+        element: <h1>HomePage</h1>
+      },
+      {
+        path: "/sobre",
+        element: <h1>Pagina Sobre</h1>
+      },
+      {
+        path: "/tarefas",
+        element: <h1>Página Tarefas</h1>
+      },
+      {
+        path: "/contato",
+        element: <h1>Página Contato</h1>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
